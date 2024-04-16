f0381ae09b0d38bc7efdc4a0ee676c698c073d84
Jacob-h-v <jvillu21@student.aau.dk>
2024-04-16 14:14:34 +0200

    CatBehaviour logic kinda

--- Assets/Scripts/CatBehaviour.cs
+++ Assets/Scripts/CatBehaviour.cs
@@ -1,18 +1,135 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CatBehaviour : MonoBehaviour
 {
+    bool mouseFound = false;
+    bool mouseInAttackRange = false;
+    bool mouseCaught = false;
+    bool isPatrolling = false;
+    bool isChasing = false;
+
+    int waypointIndex = 0;
+
+    [Header("Cat Behaviour Modifiers")]
+    [SerializeField] float timeToRotate = 2f, patrolSpeed = 5f, chaseSpeed = 8f, viewRange = 15f, viewAngle = 90f, attackRange = 1f;
+
+    [Header("References")]
+    [SerializeField] LayerMask mouseMask;
+    [SerializeField] LayerMask obstacleMask;
+    [SerializeField] UnityEngine.AI.NavMeshAgent navAgent;
+    [SerializeField] Transform[] waypoints;
+    [SerializeField] Transform mouse;
+
+    Vector3 mouseLastKnownPos = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Load and start navAgent.
+        navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        Patrol();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    bool ConditionMouseFound()
+    {
+        mouseFound = false;
+        // Create an overlap sphere to detect when mouse is close.
+        Collider[] isMouseVisible = Physics.OverlapSphere(transform.position, viewRange, mouseMask);
+        for (int i = 0; i < isMouseVisible.Length; i++)
+        {
+            Transform mouse = isMouseVisible[i].transform;
+            Vector3 directionToMouse = (mouse.position - transform.position).normalized;
+            // If mouse is in the overlap sphere and within the cat's field of view (viewAngle)
+            // and also not behind cover, set mouseFound to true.
+            if (Vector3.Angle(transform.forward, directionToMouse) < viewAngle / 2)
+            {
+                float distanceToMouse = Vector3.Distance(transform.position, mouse.position);
+                if (!Physics.Raycast(transform.position, directionToMouse, distanceToMouse, obstacleMask))
+                {
+                    mouseFound = true;
+                }
+            }
+        }
+        return mouseFound;
+    }
+
+    void Patrol()
+    {
+        isPatrolling = true;
+        isChasing = false;
+        Move(patrolSpeed);
+        // Patrol to next waypoint
+        navAgent.SetDestination(waypoints[waypointIndex].position);
+
+        // When agent approaches current waypoint, select and move to next waypoint.
+        if (navAgent.remainingDistance <= navAgent.stoppingDistance)
+        {
+            waypointIndex = (waypointIndex + 1) % waypoints.Length;
+            navAgent.SetDestination(waypoints[waypointIndex].position);
+        }
+    }
+
+
+    bool ConditonMouseInAttackRange()
+    {
+        // Check whether mouse in close enough to attack it.
+        if (Vector3.Distance(transform.position, mouse.position) <= attackRange)
+        {
+            mouseInAttackRange = true;
+        }
+        else
+        {
+            mouseInAttackRange = false;
+        }
+        return mouseInAttackRange;
+    }
+
+    void ChaseMouse()
+    {
+        isChasing = true;
+        isPatrolling = false;
+        Move(chaseSpeed);
+        // Set destination to mouse's last known position
+        navAgent.SetDestination(mouseLastKnownPos);
+
+    }
+
+    bool ConditionMouseCaught()
+    {
+        return mouseCaught;
+    }
+
+    void CatchMouse()
+    {
+        Stop();
+        mouseCaught = true;
+        // eatMouse();
+        // Win();
+
+    }
+
+
+    // navAgent functionality
+    void Move(float speed)
+    {
+        navAgent.isStopped = false;
+        navAgent.speed = speed;
+    }
+
+    void Stop()
+    {
+        navAgent.isStopped = true;
+        navAgent.speed = 0;
+    }
+
 }
